3041a80a05c549571bffa313b5a1b5d60460633b
Nadin <nmsmet@ya.ru>
2022-07-20 10:39:33 +0300

    Add folder 001_Hw

---
+++ 001_Hw/Program.cs
@@ -0,0 +1,27 @@
+﻿double[,] CreateMatr(int n, int m)
+{
+    double[,] matrix = new double[n, m];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble()*100;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatr(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("{0,8:F2}", matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+PrintMatr(CreateMatr(3, 4));

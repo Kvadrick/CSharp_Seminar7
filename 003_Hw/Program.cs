c3ca349fb830307c7068bf9f3794734ac412c404
Nadin <nmsmet@ya.ru>
2022-07-20 18:48:18 +0300

    Add folder 003_Hw

---
+++ 003_Hw/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateMatrix(int n)
+{
+    int[,] matrix = new int[n, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+void AvgMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+     
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        double avg = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            avg =  avg + matrix[j, i];
+            
+        }
+        
+        Console.WriteLine(i + 1 + " столбец: " + avg / matrix.GetLength(0) + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AvgMatrix(CreateMatrix(n));
